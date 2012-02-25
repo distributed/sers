05a5dea40a8974128bff91bd9686d500b86e5cd7
Michael Meier <distributed@gloenggi.ch>
2012-02-25 18:20:51 +0100

    new error types

--- sers.go
+++ sers.go
@@ -1,12 +1,37 @@
 package sers
 
 import (
+	"fmt"
 	"io"
 )
-
 
 type SerialPort interface {
 	io.Reader
 	io.Writer
 	io.Closer
+	SetBaudRate(br int) error
+}
+
+type StringError string
+
+func (se StringError) Error() string {
+	return string(se)
+}
+
+type ParameterError struct {
+	Parameter string
+	Reason    string
+}
+
+func (pe *ParameterError) Error() string {
+	return fmt.Sprintf("error in parameter '%s': %s")
+}
+
+type Error struct {
+	Operation       string
+	UnderlyingError error
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("%s: %v", e.Operation, e.UnderlyingError)
 }
