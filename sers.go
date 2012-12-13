b788aede430742ff7ead4d5f21d412e6130f88bd
Michael Meier <distributed@gloenggi.ch>
2012-12-13 18:36:45 +0100

    some documentation

--- sers.go
+++ sers.go
@@ -1,3 +1,6 @@
+// Package sers offers serial port access. It is a stated goal of this
+// package to allow for non-standard bit rates as the may be useful
+// in a wide range of embedded projects.
 package sers
 
 import "C"
@@ -18,11 +21,24 @@ const (
 	RTSCTS_HANDSHAKE = 1
 )
 
+// Serialport represents a serial port and offers configuration of baud
+// rate, frame format, handshaking and read paramters.
 type SerialPort interface {
 	io.Reader
 	io.Writer
 	io.Closer
+
+	// SetMode sets the frame format and handshaking configuration.
+	// baudrate may be freely chosen, the driver is allowed to reject
+	// unachievable baud rates. databits may be any number of data bits
+	// supported by the driver. parity is one of (N|O|E) for none, odd
+	// or even parity. handshake is either NO_HANDSHAKE or
+	// RTSCTS_HANDSHAKE.
 	SetMode(baudrate, databits, parity, stopbits, handshake int) error
+
+	// SetReadParams sets the minimum number of bits to read and a read
+	// timeout in seconds. These parameters roughly correspond to the
+	// UNIX termios concepts of VMIN and VTIME.
 	SetReadParams(minread int, timeout float64) error
 }
 
