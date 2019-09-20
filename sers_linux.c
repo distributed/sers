30f35c376d76c51370efd02b7252eba94655e7db
Michael Meier <distributed@gloenggi.ch>
2019-09-20 13:23:41 +0200

    add GetMode() to SerialPort

--- sers_linux.c
+++ sers_linux.c
@@ -98,6 +98,43 @@ speed_t lookupbaudrate(int br) {
 	return 0;
 }
 
+int speedtobaudrate(speed_t speed) {
+    printf("enum baud rate reverse matching\n");
+	switch (speed) {
+		case B50     : return 50      ;
+		case B75     : return 75      ;
+		case B110    : return 110     ;
+		case B134    : return 134     ;
+		case B150    : return 150     ;
+		case B200    : return 200     ;
+		case B300    : return 300     ;
+		case B600    : return 600     ;
+		case B1200   : return 1200    ;
+		case B1800   : return 1800    ;
+		case B2400   : return 2400    ;
+		case B4800   : return 4800    ;
+		case B9600   : return 9600    ;
+		case B19200  : return 19200   ;
+		case B38400  : return 38400   ;
+		case B57600  : return 57600   ;
+		case B115200 : return 115200  ;
+		case B230400 : return 230400  ;
+		case B460800 : return 460800  ;
+		case B500000 : return 500000  ;
+		case B576000 : return 576000  ;
+		case B921600 : return 921600  ;
+		case B1000000: return 1000000 ;
+		case B1152000: return 1152000 ;
+		case B1500000: return 1500000 ;
+		case B2000000: return 2000000 ;
+		case B2500000: return 2500000 ;
+		case B3000000: return 3000000 ;
+		case B3500000: return 3500000 ;
+		case B4000000: return 4000000 ;
+	}
+	return 0;
+}
+
 int setbaudrate(int fd, int br) {
 	// we try to set the baud rate via the old school interface first. this
 	// should work more reliably, if only for certain baud rates.
@@ -131,4 +168,19 @@ int setbaudrate(int fd, int br) {
 	tio.c_ospeed = br;
 	
 	return ioctl(fd, TCSETS3, &tio);
+}
+
+int getbaudrate(int fd, int *br) {
+    struct termios3 tio;
+	int ret = ioctl(fd, TCGETS3, &tio);
+	if (ret == -1) return ret;
+
+	if (tio.c_cflag & CBAUDEX) {
+	    *br = tio.c_ispeed;
+	    return 0;
+	}
+
+	*br = speedtobaudrate(tio.c_cflag & CBAUD);
+
+	return 0;
 }
--- sers_linux.go
+++ sers_linux.go
@@ -11,15 +11,32 @@ package sers
 
  extern int setbaudrate(int fd, int br);
  extern int clearnonblocking(int fd);
+ extern int getbaudrate(int fd, int *br);
 */
 import "C"
+import "fmt"
+
+type termiosPlatformData struct{}
 
 func (bp *baseport) SetBaudRate(br int) error {
-	// setting baud rate via new struct termios2 method 
+	// setting baud rate via new struct termios2 method
 	_, err := C.setbaudrate(C.int(bp.fd), C.int(br))
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (bp *baseport) getBaudrate() (int, error) {
+	var br C.int
+	ret, errnoerr := C.getbaudrate(C.int(bp.fd), &br)
+	if errnoerr != nil {
+		return 0, fmt.Errorf("error getting baud rate: %v", errnoerr)
+	}
+	if ret != 0 {
+		return 0, fmt.Errorf("error getting baud rate")
+	}
+
+	return int(br), nil
 }
